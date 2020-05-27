0cc464a4bf0368c5d71fc21851bcd5a9ef7457cb
Alfred Delle <alfred.delle@yale.edu>
2020-05-27 13:57:16 +0000

    Added webpack dev server, port 3030

--- webpack.config.js
+++ webpack.config.js
@@ -1,14 +1,27 @@
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+
 module.exports = {
   mode: "development",
   module: {
     rules: [
       {
         test: /\.(js|jsx)$/,
+        exclude: "/node_modules",
         use: {
           loader: "babel-loader",
           options: { presets: ["@babel/preset-react", "@babel/preset-env"] },
         },
       },
+      {
+        test: /\.html$/,
+        use: { loader: "html-loader" },
+      },
     ],
   },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: "./src/index.html",
+      filename: "./index.html",
+    }),
+  ],
 };
